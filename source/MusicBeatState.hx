31cd968a41f6b0faabb3da815758d10aba5968ed
8bit <139103755+RGBXD4@users.noreply.github.com>
2025-01-19 08:45:01 +0800

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -116,18 +116,18 @@ class MusicBeatState extends FlxUIState
 			remove(mobileControls);
 		}
 
-		public function addVirtualPadCamera()
+		public function addVirtualPadCamera(DefaultDrawTarget:Bool = true)
 		{
 			if (virtualPad != null)
 			{
 				var camControls:FlxCamera = new FlxCamera();
-				FlxG.cameras.add(camControls, false);
+				FlxG.cameras.add(camControls, DefaultDrawTarget);
 				camControls.bgColor.alpha = 0;
 				virtualPad.cameras = [camControls];
 			}
 		}
 		#end
-
+			
 		override function destroy()
 		{
 			#if mobile
